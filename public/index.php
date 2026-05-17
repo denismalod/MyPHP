2146a33f147e16d1da759f43fcc699a22279e7b8
Denis Malorodov <denis.malod@mail.ru>
2026-05-18 09:21:14 +1000

    Настройка соединения с БД

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
-if (!isset($_POST['firstname'])) {
-    include __DIR__ . '/../templates/form.html.php';
-} else {
-    $firstName = $_POST['firstname'];
-    $lastName = $_POST['lastname'];
-    if ($firstName == 'Tom' && $lastName == 'Butler') {
-        $output = 'Welcome, oh glorious leader!';
-    } else {
-        $output = 'Welcome to our website, ' .
-            htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
-            htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
-    }
-    include __DIR__ . '/../templates/welcome.html.php';
+try {
+    $pdo = new PDO(
+        'mysql:host=mysql;dbname=ijdb',
+        'ijdbuser',
+        'mypassword'
+    );
+    $output = 'Database connection established.';
+} catch (PDOException $e) {
+    $output = 'Unable to connect to the database server: ' . $e->getMessage();
 }
+include __DIR__ . '/../templates/output.html.php';
---
+++ templates/output.html.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Script Output</title>
+</head>
+
+<body>
+    <?php echo $output; ?>
+</body>
+
+</html>
