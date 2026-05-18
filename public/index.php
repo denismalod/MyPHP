ba9990c2afbdc72b63b3ace9ca014c896e7bc701
Denis Malorodov <denis.malod@mail.ru>
2026-05-18 16:35:33 +1000

    Вывод таблицы

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
 try {
-    $pdo = new PDO(
-        'mysql:host=mysql;dbname=ijdb',
-        'ijdbuser',
-        'mypassword'
-    );
-    $output = 'Database connection established.';
+    $pdo = new
+        PDO(
+            'mysql:host=mysql;dbname=ijdb;charset=utf8mb4',
+            'ijdbuser',
+            'mypassword'
+        );
+    $sql = 'SELECT `joketext` FROM `joke`';
+    $result = $pdo->query($sql);
+    while ($row = $result->fetch()) {
+        $jokes[] = $row['joketext'];
+    }
 } catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
+    $error = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
+        $e->getFile() . ':' . $e->getLine();
 }
-include __DIR__ . '/../templates/output.html.php';
+include __DIR__ . '/../templates/jokes.html.php';
